68dcb12fc7c23e3e101be0498435328819c416e1
benrobo <alumonabenaiah71@gmail.com>
2022-10-15 16:32:11 -0700

    updated connectmongo

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import { DB_URL } from "../config";
 
 const localUrl = "mongodb://localhost:27017/meetvast"
-// const mongoUrl  = process.env.NODE_ENV === "development" ? localUrl : DB_URL
-const mongoUrl  = process.env.NODE_ENV === "development" ? localUrl : "mongodb+srv://jobmeet:tjhVpLmktkU6pcFV@cluster0.cminvu0.mongodb.net/jobmeet?retryWrites=true&w=majority"
+const mongoUrl  = process.env.NODE_ENV === "development" ? localUrl : DB_URL
+// const mongoUrl  = process.env.NODE_ENV === "development" ? localUrl : "mongodb+srv://jobmeet:tjhVpLmktkU6pcFV@cluster0.cminvu0.mongodb.net/jobmeet?retryWrites=true&w=majority"
 
 async function connectMongo() : Promise<boolean> {
     try {
